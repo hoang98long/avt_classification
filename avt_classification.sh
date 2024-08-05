afc76e71b34913c0f008971da6306c6ef9443f58
hoang98long <53008046+hoang98long@users.noreply.github.com>
2024-08-06 00:50:19 +0700

    Update avt_classification.sh

--- avt_classification.sh
+++ avt_classification.sh
@@ -2,6 +2,7 @@
 
 CONFIG_FILE=$1
 
+# shellcheck disable=SC2164
 cd /home/avt/github/avt_classification
 conda activate avt_classify
-python main.py "$CONFIG_FILE"
+python main.py --config_file "$CONFIG_FILE"
